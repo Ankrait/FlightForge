feb096fc2e33e5ee40c332fdaea5824b18d0f5b9
Kamarrod <kamil.mirsayapov@yandex.ru>
2025-01-28 17:11:39 +0300

    feat: добавил сортировку по дате вылета

--- src/pages/Airlines/FlightsPanel/index.tsx
+++ src/pages/Airlines/FlightsPanel/index.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from 'react';
 
-import { FlightDataResponse } from '../../../store/api/flightsApi/index.types';
+import { FlightData } from '../../../store/api/flightsApi/index.types';
 import Flex from '../../../ui-kit/Flex';
 
-import Index from './FlightCard';
+import FlightCard from './FlightCard';
 
 interface FlightPanelProps {
-  flights: FlightDataResponse['data'];
+  flights: FlightData[];
 }
 
 const FlightPanel: FC<FlightPanelProps> = ({ flights }) => {
   return (
     <Flex dir="column" gap={10}>
-      {Object.values(flights).map((flight, index) => (
-        <Index key={index} flight={flight} />
+      {flights.map((flight, index) => (
+        <FlightCard key={index} flight={flight} />
       ))}
     </Flex>
   );
--- src/pages/Airlines/index.tsx
+++ src/pages/Airlines/index.tsx
@@ -1,18 +1,38 @@
 import React, { FC, useState } from 'react';
 
 import { FlightDataResponse } from '../../store/api/flightsApi/index.types';
+import Button from '../../ui-kit/Button';
 import Flex from '../../ui-kit/Flex';
 
 import FlightPanel from './FlightsPanel';
 import SearchForm from './SearchForm';
 
+
 const Airlines: FC = () => {
   const [flights, setFlights] = useState<FlightDataResponse['data'] | null>(null);
+  const [sortByDate, setSortByDate] = useState<'asc' | 'desc'>('asc');
+
+  const handleSortByDate = () => {
+    setSortByDate(prevSort => prevSort === 'asc' ? 'desc' : 'asc');
+  };
+
+  const sortedFlights = flights ? Object.values(flights).sort((a, b) => {
+    const dateA = new Date(a.depart_date).getTime();
+    const dateB = new Date(b.depart_date).getTime();
+    return sortByDate === 'asc' ? dateA - dateB : dateB - dateA;
+  }) : null;
 
   return (
     <Flex dir="column" gap={20}>
-      <SearchForm setFlights={setFlights}/>
-      {flights ? <FlightPanel flights={flights} /> : <p>Нет доступных рейсов. Попробуйте изменить параметры поиска.</p>}
+      <SearchForm setFlights={setFlights} />
+      <Button onClick={handleSortByDate}>
+        Сортировать по дате вылета ({sortByDate === 'asc' ? 'по возрастанию' : 'по убыванию'})
+      </Button>
+      {sortedFlights ? (
+        <FlightPanel flights={sortedFlights} />
+      ) : (
+        <p>Нет доступных рейсов. Попробуйте изменить параметры поиска.</p>
+      )}
     </Flex>
   );
 };
