761d65432d1e3cb72a4a927b38260d96b1bc040b
ilnaz <i.gayazov@astondevs.ru>
2025-01-31 00:10:12 +0300

    Revert "added tests for text rendering"
    This reverts commit d6dc30c4f1a128befdd6a8a2d95e8df7e6d3b8f9.

--- src/pages/Main/__test__/frontElement.test.js
+++ src/pages/Main/__test__/frontElement.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import FrontElement from '..';
+import FrontElement from '../FrontElement';
 
-test('text renders on FrontElement component', () => {
+test('text renders on Main component', () => {
   render(<FrontElement />);
 
   const frontElementText = screen.getByText(/Сервис поиска авиабилетов/i);
