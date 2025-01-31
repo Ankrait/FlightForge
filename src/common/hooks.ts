61ee38d312b148478db449eea2143306d5a5fddc
Kamarrod <kamil.mirsayapov@yandex.ru>
2025-02-01 00:22:35 +0300

    chore: удалил неиспользуемые файлы

--- src/common/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
-
-import { RootDispatchType, RootStateType } from '../store/createStore';
-
-export const useAppDispatch: () => RootDispatchType = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector;
